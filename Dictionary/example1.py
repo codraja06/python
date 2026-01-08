25c79eeae90898f6faf3685561cffdbc5d1042b0
codraja06 <rajarock1006@gmail.com>
2026-01-08 19:35:20 +0530

    Updated

---
+++ Dictionary/example1.py
@@ -0,0 +1,2 @@
+data={1:"Luffy",2:"Zoro",3:"Sanji"}
+print(data.get(4,'Not Found'))
---
+++ Dictionary/example2.py
@@ -0,0 +1,5 @@
+name=['John','Sam','Ryan']
+domain=['Python','Java','C++']
+combine=dict(zip(name,domain))
+print(combine)
+
