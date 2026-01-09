af10bbd8f02f5f307d83a8b7b290c45035ac16f4
codraja06 <rajarock1006@gmail.com>
2026-01-09 20:09:50 +0530

    Updated

--- Dictionary/example3.py
+++ Dictionary/example3.py
@@ -1,23 +1,31 @@
-user={
-    'rajaram@123':'raja@123',
-    'qwerty@123':'qwerty@123',
-    'uiop@123':'uiop@123'
+user = {
+    'rajaram@123': 'raja@123',
+    'qwerty@123': 'qwerty@123',
+    'uiop@123': 'uiop@123'
 }
-print(user)
 
-email=input("Enter the Email:")
-password=input("Enter the Password:")
+doc = input("Login or signup: ").lower()
 
-if email and password in user:
-    print(True)
-else:
+if doc == "login":
     
-    print(False)
+    email = input("Enter the Email: ")
+    password = input("Enter the Password: ")
 
+    if email in user and user[email] == password:
+        print("Login successful ✅")
+    else:
+        print("Invalid email or password ❌")
 
-"""
-if email in user:
-    print("Password:",user[email])
+elif doc == "signup":
+    
+    email = input("Enter the new Email: ")
+    password = input("Enter the new Password: ")
+
+    if email in user:
+        print("Email already exists ❌")
+    else:
+        user[email] = password
+        print("User ID added successfully ✅")
+
 else:
-    print("NOT FOUND")
-"""
+    print("Error ❌")
