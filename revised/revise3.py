204622b14115b24895df3c66671e87d3315932ac
codraja06 <rajarock1006@gmail.com>
2026-01-12 19:37:36 +0530

    updated

---
+++ revised/revise3.py
@@ -0,0 +1,13 @@
+# Find Largest & Smallest Without Built-in Functions
+numbers = [4, 7, 1, 9, 2]
+largest=numbers[0]
+smallest=numbers[0]
+
+for i in numbers:
+    if i>largest:
+        largest = i
+    if i<smallest:
+        smallest =i
+    
+print(f"Largest={largest}")
+print(f"Smallest={smallest}")
